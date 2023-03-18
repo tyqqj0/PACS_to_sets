ec09e6702acb29f6723bf1ac6c95b32c1e004c5e
tyqqj0 <tyqqj0@163.com>
2023-03-18 12:28:38 +0800

    根据数量计算

--- main.py
+++ main.py
@@ -3,12 +3,37 @@ import shutil
 import random
 
 # 定义输入输出目录和各个风格的比例
+# 照片(1670张),艺术画(2048张),动画片(2344张)和素描(3929张)
+photo = 1670
+art_painting = 2048
+cartoon = 2344
+sketch = 3929
 input_dir = "/content/gdrive/MyDrive/data/PACS_test_1_pic/PACS"
 output_dir = "/content/gdrive/MyDrive/data/PACS_train"
-train_style_ratios = {"art_painting": 0.8, "cartoon": 0.1, "photo": 0.1, "sketch": 0.0}
-val_style_ratios = {"art_painting": 0.2, "cartoon": 0.1, "photo": 0.1, "sketch": 0.6}
-test_style_ratios = {"art_painting": 0.0, "cartoon": 0.0, "photo": 0.0, "sketch": 1.0}
+# Art painting: 2048 Cartoon: 678 Photo: 678 Sketch: 784
+# Sketch: 3929 Art painting: 1217 Cartoon: 1238 Photo: 331
+# Photo: 1670 Art painting: 551 Sketch: 538 Cartoon: 466
+# Cartoon: 2344 Photo: 776 Sketch: 761 Art painting: 407
+train_style_amount = {"art_painting": 2048, "cartoon": 678, "photo": 678, "sketch": 0}
+val_style_amount = {"art_painting": 407, "cartoon": 466, "photo": 776, "sketch": 761}
+test_style_amount = {"art_painting": 0, "cartoon": 0, "photo": 0, "sketch": 784}
+train_style_ratios = {"art_painting": train_style_amount["art_painting"] / art_painting,
+                      "cartoon": train_style_amount["cartoon"] / cartoon,
+                      "photo": train_style_amount["photo"] / photo,
+                      "sketch": train_style_amount["sketch"] / sketch}
+val_style_ratios = {"art_painting": val_style_amount["art_painting"] / art_painting,
+                    "cartoon": val_style_amount["cartoon"] / cartoon,
+                    "photo": val_style_amount["photo"] / photo,
+                    "sketch": val_style_amount["sketch"] / sketch}
+test_style_ratios = {"art_painting": test_style_amount["art_painting"] / art_painting,
+                     "cartoon": test_style_amount["cartoon"] / cartoon,
+                     "photo": test_style_amount["photo"] / photo,
+                     "sketch": test_style_amount["sketch"] / sketch}
+# train_style_ratios = {"art_painting": , "cartoon": 0.1, "photo": 0.1, "sketch": 0.0}
+# val_style_ratios = {"art_painting": 0.2, "cartoon": 0.1, "photo": 0.1, "sketch": 0.6}
+# test_style_ratios = {"art_painting": 0.0, "cartoon": 0.0, "photo": 0.0, "sketch": 1.0}
 styles = ["art_painting", "cartoon", "photo", "sketch"]
+
 
 def copyd(inputdir, outputdir, ratios):
     # 创建输出目录
@@ -40,12 +65,14 @@ def copyd(inputdir, outputdir, ratios):
             for photo_name in selected_photos:
                 i += 1
                 # if(i % 100 == 0):
-                    # print("Copying {}...".format(i))
+                # print("Copying {}...".format(i))
                 photo_path = os.path.join(label_dir, photo_name)
                 output_photo_path = os.path.join(output_label_dir, photo_name)
                 shutil.copy(photo_path, output_photo_path)
             print("Finished copying {} of {}, {} photos copied.".format(label, style, select_count))
         print("Finished copying {}.\n".format(style))
+
+
 # 复制train数据集
 print("-----------------------------\nCopying train data...")
 train_output_dir = os.path.join(output_dir, "train")
